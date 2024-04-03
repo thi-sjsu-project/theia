951f93c455d9a02d58f19ba75a5c60270dfa2d63
Tom Odem <Thomas.a.odem@gmail.com>
2024-04-03 15:48:43 -0700

    detects if elements are expired

--- src/prototype/monitor.ts
+++ src/prototype/monitor.ts
@@ -1,5 +1,10 @@
+import { useAppDispatch, useAppSelector } from '../redux/hooks';
+import { addWidget, getWidgetById, getWidgets } from '../redux/slices/cmSlice';
+import type { Widget } from '../types/modalities';
+
 type MonitorProps = {
   // define expected input here and it's type (number, string, etc.)
+  widgets: Widget[]
 };
 
 /**
@@ -7,8 +12,17 @@ type MonitorProps = {
  * @param ???
  * @returns ???
  */
-const monitor = ({}: MonitorProps) => {
-  console.log('monitor');
+const monitor = ({widgets}: MonitorProps) => {
+  console.log('monitor went off!');
+
+  widgets.forEach(function(widget,widgetIndex){
+    widget.elements.forEach(function(element, elementIndex) {
+      const time = new Date().toISOString();
+      if(element.expiration <= time){
+        console.log("element " + element.id + " expired!")
+      }
+    });
+  });
 };
 
 export default monitor;
