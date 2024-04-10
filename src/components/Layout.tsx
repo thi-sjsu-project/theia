9d454767c9a5e99fb91fc4a1e352e7eb8cc688f0
bedlam343 <jagik22@gmail.com>
2024-04-10 13:26:31 -0700

    add ownship and drone icons

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,12 +1,14 @@
+import { FaLocationArrow } from 'react-icons/fa';
+import { GiDeliveryDrone } from 'react-icons/gi';
 import GridLayout from 'react-grid-layout';
 import type { Layout as LayoutType } from 'react-grid-layout';
 
 const Layout = () => {
   const layout: LayoutType[] = [
     { i: 'a', x: 0, y: 0, w: 300, h: 1080, static: true },
-    { i: 'b', x: 500, y: 0, w: 300, h: 50 },
-    { i: 'c', x: 1500, y: 300, w: 200, h: 80 },
-    { i: 'd', x: 450, y: 500, w: 250, h: 80 },
+    { i: 'drone1', x: 500, y: 200, w: 50, h: 50 },
+    { i: 'drone2', x: 1500, y: 300, w: 50, h: 50 },
+    { i: 'ownship', x: 800, y: 500, w: 50, h: 50 },
   ];
 
   return (
@@ -31,22 +33,14 @@ const Layout = () => {
           >
             <p className="text-5xl">Tinder Box</p>
           </div>
-          <div key="b" className="bg-green-300">
-            I DONT KNOW WHAT TO PUT HERE...
+          <div key="drone1">
+            <GiDeliveryDrone size={50} />
           </div>
-          <div
-            key="c"
-            className="bg-blue-500 flex 
-            items-center justify-center"
-          >
-            Message...
+          <div key="drone2">
+            <GiDeliveryDrone size={50} />
           </div>
-          <div
-            key="d"
-            className="bg-red-500 flex 
-            items-center justify-center"
-          >
-            DANGER!!!
+          <div key="ownship">
+            <FaLocationArrow size={50} />
           </div>
         </GridLayout>
       </div>
