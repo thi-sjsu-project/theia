87f9d61001f0c897689bb04aafbaae204ba89bda
Elena Schramme <elena.schramme@orbem.ai>
2024-04-11 11:31:56 -0700

    add widget existing check for assimilator

--- src/prototype/assimilator.ts
+++ src/prototype/assimilator.ts
@@ -13,7 +13,7 @@ type AssimilatorProps = {
  * @returns ???
  */
 const assimilator = ({ possibleWidgets, pixelMap }: AssimilatorProps) => {
-  let widgetToDeploy = null; //will return null if we cannot find a space
+  let widgetToDeploy: Widget | null = null; //will return null if we cannot find a space
   pixelMap.forEach(function (row, rowIndex) {
     //go through each row
     row.forEach(function (cell, colIndex) {
@@ -21,7 +21,16 @@ const assimilator = ({ possibleWidgets, pixelMap }: AssimilatorProps) => {
       if (cell.widgetIDs.length === 0) {
         //found space to put the widget in
         widgetToDeploy = possibleWidgets[0]; //the widget can be deployed
-        //widgetToDeploy.location = [rowIndex, colIndex]; //set the location of where it will be placed
+
+        //check if the cell already contains a widget with the same ID as widgetToDeploy
+        const existingWidgetIndex = cell.widgetIDs.findIndex(id => id === widgetToDeploy?.id);
+        if (existingWidgetIndex !== -1) {
+          console.log(`Updated widget ${widgetToDeploy.id} since it already exists.`);
+          //update existing widget
+          widgetToDeploy = null;
+        } else {
+          //widgetToDeploy.location = [rowIndex, colIndex]; //set the location of where it will be placed
+        }
       }
     });
   });
