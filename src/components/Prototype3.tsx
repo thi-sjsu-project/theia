898693dcc6c6733205420bdee29e51912d3f3547
bedlam343 <jagik22@gmail.com>
2024-04-18 21:42:02 -0700

    move useMousePointer inside hooks folder

--- src/components/Prototype3.tsx
+++ src/components/Prototype3.tsx
@@ -2,11 +2,12 @@ import { useState } from 'react';
 import Layout from './Layout';
 import { useAppSelector } from 'src/redux/hooks';
 import { getSections, getWidgets } from 'src/redux/slices/minimapSlice';
-import { useMousePosition } from "src/utils/useMousePosition"
+import { useMousePosition } from 'src/hooks/useMousePosition';
 
 const Prototype3 = () => {
-  const mousePosition = useMousePosition()
-  console.log(mousePosition)
+  const mousePosition = useMousePosition();
+  console.log(mousePosition);
+
   const [messages, setMessages] = useState<string[]>([]);
 
   const sections = useAppSelector(getSections);
