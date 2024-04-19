e2886915b81967c5b0d9531f691b84029f41c28d
bedlam343 <jagik22@gmail.com>
2024-04-18 21:47:29 -0700

    add typing to useMousePosition

--- src/hooks/useMousePosition.ts
+++ src/hooks/useMousePosition.ts
@@ -1,25 +1,34 @@
-import { useEffect, useState } from "react"
+import { useEffect, useState } from 'react';
+
+type MousePosition = {
+  x: number | undefined;
+  y: number | undefined;
+};
+
+const initialState: MousePosition = {
+  x: undefined,
+  y: undefined,
+};
 
 export function useMousePosition() {
-  const [mousePosition, setMousePosition] = useState({
-    x: null,
-    y: null,
-  })
+  const [mousePosition, setMousePosition] = useState<MousePosition>({
+    ...initialState,
+  });
 
   useEffect(() => {
-    const updateMousePosition = (ev) => {
+    const updateMousePosition = (ev: MouseEvent) => {
       setMousePosition({
         x: ev.clientX,
         y: ev.clientY,
-      })
-    }
+      });
+    };
 
-    window.addEventListener("mousemove", updateMousePosition)
+    window.addEventListener('mousemove', updateMousePosition);
 
     return () => {
-      window.removeEventListener("mousemove", updateMousePosition)
-    }
-  }, [])
+      window.removeEventListener('mousemove', updateMousePosition);
+    };
+  }, []);
 
-  return mousePosition
+  return mousePosition;
 }
