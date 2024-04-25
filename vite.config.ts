946c3728b962b3fdabb0e31386f22f7e4dd836d2
bedlam343 <jagik22@gmail.com>
2024-04-25 13:26:35 -0700

    vite-plugin-checker for checking type errors

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,19 @@
 import { defineConfig } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import eslint from 'vite-plugin-eslint';
+import checker from 'vite-plugin-checker';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), eslint()],
+  plugins: [
+    react(),
+    eslint(),
+    checker({
+      typescript: true,
+      // eslint: { lintCommand: 'eslint src' },
+      overlay: false,
+    }),
+  ],
   server: {
     open: true,
   },
