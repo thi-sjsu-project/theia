09122a55cef096cf3e395ba8821b6a274f3c8572
bedlam343 <jagik22@gmail.com>
2024-04-27 13:13:51 -0700

    change

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,7 @@
 import 'src/App.css';
-import { useState } from 'react';
 import Prototype3 from 'src/components/Prototype3';
-import Button from 'src/ui/Button1';
 
 const App = () => {
-  const [playDemo, setPlayDemo] = useState(false);
-
-  if (!playDemo) {
-    return (
-      <div className="h-screen flex items-center justify-center">
-        <Button
-          onClick={() => setPlayDemo(true)}
-          text="START AMAZING DEMO!!!"
-        />
-      </div>
-    );
-  }
-
   return <Prototype3 />;
 };
 
