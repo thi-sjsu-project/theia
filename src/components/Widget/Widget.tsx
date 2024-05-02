bc396dd1ff9850eaa3767274ac9a7f61a4c7f181
polpol <paul.chon@sjsu.edu>
2024-05-01 22:40:12 -0700

    fix placing widget on screen to absolute

--- src/components/Widget/Widget.tsx
+++ src/components/Widget/Widget.tsx
@@ -15,21 +15,30 @@ const Widget = ({ widget }: WidgetProps) => {
       case 'tinder':
         return <TinderWidget widget={widget} />;
       case 'highWarning':
-        return <div style={{
-        height: widget.h,
-        width: widget.w,
-        top: widget.y,
-        left: widget.x,
-        ...widget.style,
-        }}>
-          <p style={{
-            height: widget.elements[0].h,
-            width: widget.elements[0].w,
-            top: widget.elements[0].yWidget,
-            left: widget.elements[0].xWidget,
-            ...widget.elements[0].style,
-          }}>{'text' in widget.elements[0] && widget.elements[0].text}</p>
-        </div>;
+        return (
+          <div
+            className="absolute"
+            style={{
+              height: widget.h,
+              width: widget.w,
+              top: widget.y,
+              left: widget.x,
+              ...widget.style,
+            }}
+          >
+            <p
+              style={{
+                height: widget.elements[0].h,
+                width: widget.elements[0].w,
+                top: widget.elements[0].yWidget,
+                left: widget.elements[0].xWidget,
+                ...widget.elements[0].style,
+              }}
+            >
+              {'text' in widget.elements[0] && widget.elements[0].text}
+            </p>
+          </div>
+        );
       default:
         return <div>Unknown Widget</div>;
     }
