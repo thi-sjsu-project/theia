ad5ee9b1a7f169e8b4ea04d1d98569c58ec106e8
Azlothe <bbqian7@gmail.com>
2024-05-09 23:00:35 -0700

    replace range and measure with ModalityMeasure type

--- src/prototype/restrainer.ts
+++ src/prototype/restrainer.ts
@@ -1,7 +1,20 @@
 import store from 'src/redux/store';
 import type { Modality } from 'src/types/modalities';
 import { type Widget } from 'src/types/widget';
-import { type ModalityMeasureRange, type ModalityMeasureBoundary } from 'src/types/restrain';
+import { type ModalityMeasure } from 'src/types/restrain';
+
+
+
+
+// for now it only takes into account how many visual and audio modalities are used
+/**
+ * Note:
+ * or use array instead of number and use objects with attributes such as volume, frequency
+ * amount of audios playing at the same time for example can be seen by the amout of objects in the list
+ */
+
+
+
 
 type RestrainerProps = {
   // define expected input here and it's type (number, string, etc.)
@@ -9,17 +22,19 @@ type RestrainerProps = {
   // add more as needed
 };
 
-export type ModalityMeasure = {
-  // for now it only takes into account how many visual and audio modalities are used
-  /**
-   * or use array instead of number and use objects with attributes such as volume, frequency
-   * amount of audios playing at the same time for example can be seen by the amout of objects in the list
-   */
-  type: Modality;
-  measure: number;
-  range: ModalityMeasureRange;
-  boundary: ModalityMeasureBoundary;
-};
+// **** Set modality boundaries and ranges ****
+const visualMeasure : ModalityMeasure = {
+  type: "visual",
+  // measure: 0,
+  range: {
+    min: 0,
+    max: 100,
+  },
+  boundary: {
+    min: 0,
+    max: 80,
+  },
+}
 
 // **** Simulated restrainer visual complexity ****
 function totalVisualComplexity(widget : Widget) {
@@ -29,20 +44,10 @@ function totalVisualComplexity(widget : Widget) {
   // Calculating the visual complexity of the widgetToDeploy
   const widgetVisualComplexity = (widget : Widget) => widget?.elements.length;
 
-  return Math.min(visualRange.max, Math.max(visualRange.min, currentScreenComplexity() + widgetVisualComplexity(widget))); // keep the complexity within range bounds
+  return Math.min(visualMeasure.range.max, Math.max(visualMeasure.range.min, currentScreenComplexity() + widgetVisualComplexity(widget))); // keep the complexity within range bounds
 }
 
 
-// **** Set modality boundaries and ranges ****
-const visualRange : ModalityMeasureRange = {
-  min: 0,
-  max: 100
-};
-
-const visualBound : ModalityMeasureBoundary = {
-  min: 0,
-  max: 80,
-};
 
 /**
  * @description restrains the cm if placing the widgetToDeploy will cause to exceed our maximum threshold
@@ -52,10 +57,10 @@ const visualBound : ModalityMeasureBoundary = {
 const restrainer = ({ widgetToDeploy }: RestrainerProps) => {
   // Calculating if adding @param widget will remain within bounds
   const visualComplexityAfterAddingWidget = totalVisualComplexity(widgetToDeploy);
-  const canBePlaced = visualBound.min <= visualComplexityAfterAddingWidget && visualComplexityAfterAddingWidget <= visualBound.max;
+  const canBePlaced = visualMeasure.boundary.min <= visualComplexityAfterAddingWidget && visualComplexityAfterAddingWidget <= visualMeasure.boundary.max;
 
   if (!canBePlaced) 
-    console.warn(`This widget could not be deployed. Adding the widget will result in visual complexity of ${visualComplexityAfterAddingWidget}. Will go out of acceptable bounds ${visualBound.min} - ${visualBound.max}.`)
+    console.warn(`This widget could not be deployed. Adding the widget will result in visual complexity of ${visualComplexityAfterAddingWidget}. Will go out of acceptable bounds ${visualMeasure.boundary.min} - ${visualMeasure.boundary.max}.`)
 
   return canBePlaced;
 };
--- src/types/restrain.ts
+++ src/types/restrain.ts
@@ -11,13 +11,8 @@ type ModalityMeasureBoundary = {
 };
 
 export type ModalityMeasure = {
-  // for now it only takes into account how many visual and audio modalities are used
-  /**
-   * or use array instead of number and use objects with attributes such as volume, frequency
-   * amount of audios playing at the same time for example can be seen by the amout of objects in the list
-   */
   type: Modality;
-  measure: number;
+  measure?: number;
   range: ModalityMeasureRange;
   boundary: ModalityMeasureBoundary;
 };
