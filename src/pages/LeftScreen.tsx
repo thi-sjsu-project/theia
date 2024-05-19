885e1696d769c64fbe897a006cb62122e7a12efd
thetek <git@thetek.de>
2024-05-19 20:33:17 +0200

    fix whatever went wrong with the top bar

--- src/pages/LeftScreen.tsx
+++ src/pages/LeftScreen.tsx
@@ -21,7 +21,7 @@ const LeftScreen = () => {
   return (
     <div className="absolute top-0 left-0 bg-[#1E1E1E] w-[1920px] h-[1080px] hover:cursor-pointer">
       {/* Top Bar */}
-      <div className="absolute min-w-[1920px] border-2 border-b-stone-800 min-h-[100px]" />
+      <div className="absolute w-[1920px] bg-[#252526] h-[100px] shadow-lg shadow-[#181818]" />
 
       {/* Maybe just render a WidgetList component? Or a ListWidget component? */}
       {Object.keys(widgets).map((widgetId) => (
