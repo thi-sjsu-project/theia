887e2f7518bfded96255981b901e3e01b33b4015
Azlothe <bbqian7@gmail.com>
2024-05-19 21:29:13 -0700

    extract color into props

--- src/components/Element/Simple/TableElement.tsx
+++ src/components/Element/Simple/TableElement.tsx
@@ -6,9 +6,21 @@ type TableElementProps = {
   // children for nested elements that Tom mentioned.
   children?: ReactNode;
   // and more props to perhaps control position of the children
+
+  borderColor?: string;
+  evenAlternatingColor?: string;
+  oddAlternatingColor?: string;
+  leftLabelColor?: string;
 };
 
-const TableElement = ({ element, children }: TableElementProps) => {
+const TableElement = ({
+  element,
+  children,
+  borderColor = '#585864',
+  evenAlternatingColor = '#202021',
+  oddAlternatingColor = '#00FFFFFF',
+  leftLabelColor = '#bcbcbc',
+}: TableElementProps) => {
   const { rows, cols, tableData } = element;
 
   const renderTable = () => {
@@ -17,13 +29,13 @@ const TableElement = ({ element, children }: TableElementProps) => {
         <tbody>
           {Array.from({ length: rows }).map((_, i) => (
             <tr
-              className={`border-y-2 border-[#585864] ${i % 2 === 0 && 'bg-[#202021]'}`}
+              className={`border-y-2 border-[${borderColor}] bg-[${i % 2 === 0 ? evenAlternatingColor : oddAlternatingColor}] `}
               key={i}
             >
               {Array.from({ length: cols }).map((_, j) => (
                 <td
                   key={j}
-                  className={`${j > 0 ? 'border-l-2' : 'border-x-0'} ${j === 0 && 'text-[#bcbcbc]'} border-[#585864] py-1`}
+                  className={`${j > 0 ? 'border-l-2' : 'border-x-0'} ${j === 0 && `text-[${leftLabelColor}]`} border-[${borderColor}] py-1`}
                   style={{ width: `${100 / cols}%` }}
                 >
                   <div className="ml-6">{tableData[i][j]}</div>
