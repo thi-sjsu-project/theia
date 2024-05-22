a977a962cb33f80f44b5269e550aa4e1a4be13e5
polpol <paul.chon@sjsu.edu>
2024-05-22 16:10:39 -0700

    add fake history for L widget and try to fix gaze highlight but still weird

--- src/components/Element/Complex/MapThreatInfoElement.tsx
+++ src/components/Element/Complex/MapThreatInfoElement.tsx
@@ -99,49 +99,74 @@ const MapThreatInfoElement = ({ element, inGaze }: Props) => {
 
       case 'L':
         return (
-          <div className="grid auto-row-auto gap-y-[10px] p-[15px_20px_30px_30px] text-white bg-[#282828] bg-opacity-90 rounded-xl">
-            <div className="font-medium text-4xl mb-[5px]">SA_4 Air Defense Threat</div>
-            <div className="grid grid-cols-[40px_1fr]">
-              <div className="flex-auto bg-turquoise text-black text-2xl text-center h-fit font-semibold rounded-l-md">
-                4
+          <>
+            <div
+              className={
+                inGaze
+                  ? 'grid auto-row-auto gap-y-[5px] p-[15px_20px_30px_30px] text-white bg-[#282828] bg-opacity-90 rounded-xl border-turquoise border-x-[5px] '
+                  : 'grid auto-row-auto gap-y-[5px] p-[15px_20px_30px_30px] text-white bg-[#282828] bg-opacity-90 rounded-xl'
+              }
+            >
+              {/* <div className="grid auto-row-auto gap-y-[5px] p-[15px_20px_30px_30px] text-white bg-[#282828] bg-opacity-90 rounded-xl"> */}
+              <div className="font-medium text-4xl mb-[5px]">
+                SA_4 Air Defense Threat
               </div>
+              <div className="grid grid-cols-[40px_1fr]">
+                <div className="flex-auto bg-turquoise text-black text-2xl text-center h-fit font-semibold rounded-l-md">
+                  4
+                </div>
 
-              <div className="bg-convo-bg h-fit p-4 rounded-lg drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)]">
-                <div className="grid grid-cols-1">
-                  <div className="col-span-2 flex flex-col">
-                    <div className="font-medium text-[28px]">
-                      ACA-7: Request to attack
+                <div className="bg-convo-bg h-fit p-4 rounded-lg drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)]">
+                  <div className="grid grid-cols-1">
+                    <div className="col-span-2 flex flex-col">
+                      <div className="font-medium text-[28px]">
+                        ACA-7: Request to attack
+                      </div>
+                      <div className="font-2xl">Kinetic attack approval</div>
                     </div>
-                    <div className="font-2xl">
-                      Kinetic attack approval
+
+                    <div className="col-span-4 text-left">
+                      <TableElement
+                        element={{
+                          id: 'convoID_<el.index>',
+                          modality: 'visual',
+                          h: 3,
+                          w: 4,
+
+                          type: 'table',
+                          rows: 4,
+                          cols: 2,
+                          tableData: [
+                            ['location', '34N, 118W'],
+                            ['priority', 'high'],
+                            ['threat-level', 'high'],
+                            ['col.damage', '45%'],
+                          ],
+                        }}
+                      />
                     </div>
-                  </div>
-
-                  <div className="col-span-4 text-left">
-                    <TableElement
-                      element={{
-                        id: 'convoID_<el.index>',
-                        modality: 'visual',
-                        h: 3,
-                        w: 4,
-
-                        type: 'table',
-                        rows: 4,
-                        cols: 2,
-                        tableData: [
-                          ['location', '34N, 118W'],
-                          ['priority', 'high'],
-                          ['threat-level', 'high'],
-                          ['col.damage', '45%'],
-                        ],
-                      }}
-                    />
                   </div>
                 </div>
               </div>
+              <div className="grid grid-cols-[40px_1fr]">
+                <div className="flex-auto bg-[#3E3E42] text-white/50 text-2xl text-center h-fit font-normal rounded-l-md">
+                  3
+                </div>
+                <div className="bg-[#252526] text-[#BCBCBC] px-[20px] py-[5px]">
+                  Update: threat-level high
+                </div>
+              </div>
+              <div className="grid grid-cols-[40px_1fr]">
+                <div className="flex-auto bg-[#3E3E42] text-white/50 text-2xl text-center h-fit font-normal rounded-l-md">
+                  2
+                </div>
+                <div className="bg-[#252526] text-[#BCBCBC] px-[20px] py-[5px]">
+                  Update: location changed
+                </div>
+              </div>
             </div>
-            {inGaze ? <GazeHighlightL /> : <></>}
-          </div>
+            {/* {inGaze ? <GazeHighlightL /> : <></>} */}
+          </>
           // <div
           //   id={element.id}
           //   style={{
