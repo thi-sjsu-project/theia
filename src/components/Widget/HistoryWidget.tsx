9ce9389ecb3ed2e5f9b51d12f58afb0043fcb918
bedlam343 <jagik22@gmail.com>
2024-05-25 15:48:02 -0700

    empty history widget message

--- src/components/Widget/HistoryWidget.tsx
+++ src/components/Widget/HistoryWidget.tsx
@@ -23,6 +23,41 @@ const HistoryWidget = ({ widget }: HistoryWidgetProps) => {
   //   (message) => message.id === activeElementID,
   // );
 
+  const renderHistory = () => {
+    if (numMessages) {
+      return (
+        <div className="grid grid-cols-12 items-start justify-start p-4 gap-4">
+          {convoMessages.reverse().map((message, index) => (
+            <>
+              <div key={message.id} className="col-span-1 flex flex-col h-full">
+                <MessageNumber number={numMessages - index} glow />
+
+                {/* line below the number */}
+                {numMessages - index !== 1 && (
+                  <div className="border-convo-bar h-full w-1/2 border-r-4 mt-4 flex flex-row items-center justify-center rounded-sm" />
+                )}
+              </div>
+
+              <HistoryElement
+                key={`${message.id}-${index}`}
+                message={message}
+                index={index + 1}
+                outerDivStyleClass="col-span-11 bg-convo-bg rounded-lg p-2 h-fit
+          drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)]"
+              />
+            </>
+          ))}
+        </div>
+      );
+    }
+
+    return (
+      <div className="flex items-center justify-center h-full w-full text-xl">
+        Select a conversation to view messages
+      </div>
+    );
+  };
+
   return (
     <div
       style={{
@@ -33,28 +68,7 @@ const HistoryWidget = ({ widget }: HistoryWidgetProps) => {
       }}
       className="absolute bg-[#323235] text-white"
     >
-      <div className="grid grid-cols-12 items-start justify-start p-4 gap-4">
-        {convoMessages.reverse().map((message, index) => (
-          <>
-            <div key={message.id} className="col-span-1 flex flex-col h-full">
-              <MessageNumber number={numMessages - index} glow />
-
-              {/* line below the number */}
-              {numMessages - index !== 1 && (
-                <div className="border-convo-bar h-full w-1/2 border-r-4 mt-4 flex flex-row items-center justify-center rounded-sm" />
-              )}
-            </div>
-
-            <HistoryElement
-              key={`${message.id}-${index}`}
-              message={message}
-              index={index + 1}
-              outerDivStyleClass="col-span-11 bg-convo-bg rounded-lg p-2 h-fit
-          drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)]"
-            />
-          </>
-        ))}
-      </div>
+      {renderHistory()}
     </div>
   );
 };
