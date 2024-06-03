fcaf549e1d9152588ae9f51d8a8bc890828de846
Polfish <hispjyc@gmail.com>
2024-06-03 06:06:29 -0700

    highlight unread/unhighlight read messages in history widget

--- src/components/Widget/HistoryWidget.tsx
+++ src/components/Widget/HistoryWidget.tsx
@@ -10,7 +10,7 @@ import {
   getCommunication,
 } from 'src/redux/slices/communicationSlice';
 import MessageNumber from 'src/ui/history/MessageNumber';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 type HistoryWidgetProps = {
   widget: HistoryWidgetType;
@@ -21,10 +21,13 @@ const HistoryWidget = ({ widget }: HistoryWidgetProps) => {
   const activeConversationId = useAppSelector(getActiveConversationId);
 
   const dispatch = useAppDispatch();
-  const conversation = useAppSelector((state) =>
-    getConversation(state, activeConversationId),
-  );
-  const messages = conversation?.messages ? [...conversation.messages] : [];
+  const conversation =
+    useAppSelector((state) => getConversation(state, activeConversationId)) ||
+    {};
+
+  const { messages = [], numUnreadMessages = 0 } = conversation;
+  const [unreadCount, setUnreadCount] = useState(0);
+
   const numMessages = messages.length || 0;
 
   // useEffect(() => {
@@ -39,39 +42,54 @@ const HistoryWidget = ({ widget }: HistoryWidgetProps) => {
 
   // don't show unread counter for newly arriving messages if conversation is open in history widget
   useEffect(() => {
-    if (conversation?.numUnreadMessages > 0) {
-      dispatch(updateNumUnreadMessages(conversation.id, 0));
+    if (numUnreadMessages > 0) {
+      setUnreadCount(numUnreadMessages);
+      dispatch(updateNumUnreadMessages(activeConversationId, 0));
+    } else if (numUnreadMessages === 0) {
+      setUnreadCount(0);
     }
-  }, [conversation?.numUnreadMessages, conversation?.id, dispatch]);
+  }, [activeConversationId, dispatch]);
+
+  if (!conversation) {
+    return null;
+  }
 
   const renderHistory = () => {
     if (numMessages) {
       return (
         <div className="grid grid-cols-12 items-start justify-start p-4 gap-4">
-          {messages.map((message, index) => (
-            <>
-              <div key={message.id} className="col-span-1 flex flex-col h-full">
-                <MessageNumber
-                  key={`${message.id}-number`}
-                  number={numMessages - index}
-                  glow={index === 0}
-                />
+          {messages.map((message, index) => {
+            return (
+              <>
+                <div
+                  key={message.id}
+                  className="col-span-1 flex flex-col h-full"
+                >
+                  <MessageNumber
+                    key={`${message.id}-number`}
+                    number={numMessages - index}
+                    glow={index < unreadCount}
+                  />
 
-                {/* line below the number */}
-                {numMessages - index !== 1 && (
-                  <div key={`${message.id}-line`} className="border-convo-bar h-full w-1/2 border-r-4 mt-4 flex flex-row items-center justify-center rounded-sm" />
-                )}
-              </div>
+                  {/* line below the number */}
+                  {numMessages - index !== 1 && (
+                    <div
+                      key={`${message.id}-line`}
+                      className="border-convo-bar h-full w-1/2 border-r-4 mt-4 flex flex-row items-center justify-center rounded-sm"
+                    />
+                  )}
+                </div>
 
-              <HistoryElement
-                key={`${message.id}-${index}`}
-                message={message}
-                index={index + 1}
-                outerDivStyleClass="col-span-11 bg-convo-bg rounded-lg p-2 h-fit
+                <HistoryElement
+                  key={`${message.id}-${index}`}
+                  message={message}
+                  index={index + 1}
+                  outerDivStyleClass="col-span-11 bg-convo-bg rounded-lg p-2 h-fit
           drop-shadow-[0_4px_4px_rgba(0,0,0,0.5)]"
-              />
-            </>
-          ))}
+                />
+              </>
+            );
+          })}
         </div>
       );
     }
